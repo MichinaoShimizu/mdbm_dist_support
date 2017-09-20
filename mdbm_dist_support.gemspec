ef8330a481f216a9d4b6bfe4a276b318eafa9b21
MichinaoShimizu <shimizu.michinao@gmail.com>
2017-09-20 23:37:53 +0900

    rspec

--- mdbm_dist_support.gemspec
+++ mdbm_dist_support.gemspec
@@ -19,5 +19,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.14'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_runtime_dependency     'simplecov', '>= 0.15'
   spec.add_runtime_dependency     'ruby-mdbm', '>= 0.0.2'
 end
--- spec/mdbm_dist_support_spec.rb
+++ spec/mdbm_dist_support_spec.rb
@@ -1,30 +1,38 @@
 require "spec_helper"
 
-RSpec.describe MdbmDistSupport do
-  it 'has a version number' do
-    expect(MdbmDistSupport::VERSION).not_to be nil
+describe MdbmDistSupport::Validator do
+  subject { MdbmDistSupport::Validator }
+  describe 'valid_run_dist_settings?()' do
+    context 'case: all of require instance variables exist' do
+      it 'valid. return true' do
+        expect(subject.valid_run_dist_settings?(subject::RUN_DIST_REQUIRE_INSTANCE_VARS)).to eq(true)
+      end
+    end
+
+    context 'case: some of require instance variables not exist' do
+      it 'invalid. return false' do
+        expect(subject.valid_run_dist_settings?([:@lock_path])).to eq(false)
+      end
+    end
+  end
+
+  describe 'valid_run_print_after_settings?()' do
+    context 'case: all of require instance variables exist' do
+      it 'valid. return true' do
+        expect(subject.valid_run_print_after_settings?(subject::RUN_PRINT_AFTER_REQUIRE_INSTANCE_VARS)).to eq(true)
+      end
+    end
+
+    context 'case: some of require instance variables not exist' do
+      it 'invalid. return false' do
+        expect(subject.valid_run_dist_settings?([:@lock_path])).to eq(false)
+      end
+    end
   end
 end
 
-RSpec.describe MdbmDistSupport::Validator do
-  it 'MdbmDistSupport::Validator::valid_run_dist_settings? valid case' do
-    expect(MdbmDistSupport::Validator::valid_run_dist_settings?(MdbmDistSupport::Validator::RUN_DIST_REQUIRE_INSTANCE_VARS)).to eq(true)
-  end
-
-  it 'MdbmDistSupport::Validator::valid_run_dist_settings? invalid case' do
-    expect(MdbmDistSupport::Validator::valid_run_dist_settings?([:@lock_path])).to eq(false)
-  end
-
-  it 'MdbmDistSupport::Validator::valid_run_print_after_settings? valid case' do
-    expect(MdbmDistSupport::Validator::valid_run_print_after_settings?(MdbmDistSupport::Validator::RUN_PRINT_AFTER_REQUIRE_INSTANCE_VARS)).to eq(true)
-  end
-
-  it 'MdbmDistSupport::Validator::valid_run_print_after_settings? invalid case' do
-    expect(MdbmDistSupport::Validator::valid_run_dist_settings?([:@lock_path])).to eq(false)
-  end
-end
-
-RSpec.describe MdbmDistSupport::Lock do
+describe MdbmDistSupport::Lock do
+  subject { MdbmDistSupport::Lock }
   before do
     @path = '/tmp/UT-MdbmDistSupport.lck'
   end
@@ -33,18 +41,24 @@ RSpec.describe MdbmDistSupport::Lock do
     File.unlink @path
   end
 
-  it 'MdbmDistSupport::Lock::try_lock success case' do
-    expect(MdbmDistSupport::Lock.new(@path).try_lock).to eq(true)
-  end
+  describe 'try_lock()' do
+    context 'case: single proc call' do
+      it 'no error. return true' do
+        expect(subject.new(@path).try_lock).to eq(true)
+      end
+    end
 
-  it 'MdbmDistSupport::Lock::try_lock duplicate lock case' do
-    path = '/tmp/UT-MdbmDistSupport.lck'
-    MdbmDistSupport::Lock.new(@path).try_lock
-    expect(MdbmDistSupport::Lock.new(@path).try_lock).to eq(false)
+    context 'case: 2 proc open call' do
+      it 'cannot get lock. return false' do
+        subject.new(@path).try_lock
+        expect(subject.new(@path).try_lock).to eq(false)
+      end
+    end
   end
 end
 
-RSpec.describe MdbmDistSupport::Meta do
+describe MdbmDistSupport::Meta do
+  subject { MdbmDistSupport::Meta }
   before do
     @path = '/tmp/UT-MdbmDistSupport.meta'
   end
@@ -53,32 +67,20 @@ RSpec.describe MdbmDistSupport::Meta do
     File.unlink @path
   end
 
-  it 'MdbmDistSupport::Meta::fetch string case' do
-    meta = MdbmDistSupport::Meta.new @path
-    meta.store('hoge', 'fuga')
-    expect(meta.fetch('hoge')).to eq('fuga')
-  end
+  describe 'fetch()' do
+    context 'case: key exist' do
+      it 'return setted string object' do
+        meta = subject.new @path
+        meta.store('hoge', 'fuga')
+        expect(meta.fetch('hoge')).to eq('fuga')
+      end
+    end
 
-  it 'MdbmDistSupport::Meta::fetch nil case' do
-    meta = MdbmDistSupport::Meta.new @path
-    expect(meta.fetch('fuga')).to eq(nil)
-  end
-
-  it 'MdbmDistSUpport::MEta::store string case save string' do
-    meta = MdbmDistSupport::Meta.new @path
-    meta.store('hoge', 'fuga')
-    expect(meta.fetch('hoge').class).to eq(String)
-  end
-
-  it 'MdbmDistSUpport::MEta::store nil case save string' do
-    meta = MdbmDistSupport::Meta.new @path
-    meta.store('hoge', nil)
-    expect(meta.fetch('hoge').class).to eq(String)
-  end
-
-  it 'MdbmDistSUpport::MEta::store time case save string' do
-    meta = MdbmDistSupport::Meta.new @path
-    meta.store('hoge', Time.new)
-    expect(meta.fetch('hoge').class).to eq(String)
+    context 'case: key not exist' do
+      it 'return nil' do
+        meta = subject.new @path
+        expect(meta.fetch('hoge')).to eq(nil)
+      end
+    end
   end
 end
