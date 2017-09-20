03951443109a6bb0867d29bfa68bd02372495a55
MichinaoShimizu <shimizu.michinao@gmail.com>
2017-09-20 23:45:17 +0900

    spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
 require "bundler/setup"
 require "simplecov"
-require "mdbm_dist_support"
+SimpleCov.start
+require 'mdbm_dist_support'
 
 RSpec.configure do |config|
-  SimpleCov.start
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
   config.expect_with :rspec do |c|
     c.syntax = :expect
